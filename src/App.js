28ee405aec93611c1f67ad0581341378a0dc3503
Zaidm <1337zain1337@mail.ru>
2023-05-18 19:07:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Squares extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color1: 'red',
+      color2: 'blue'
+    };
+  }
+
+  swapColors = () => {
+    const { color1, color2 } = this.state;
+    this.setState({
+      color1: color2,
+      color2: color1
+    });
+  };
+
+  render() {
+    const { color1, color2 } = this.state;
+
+    return (
+      <div className="container">
+        <div
+          className="square"
+          style={{ backgroundColor: color1 }}
+          onClick={this.swapColors}
+        ></div>
+        <div
+          className="square"
+          style={{ backgroundColor: color2 }}
+          onClick={this.swapColors}
+        ></div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Squares;
